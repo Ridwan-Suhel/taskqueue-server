6dd4d94486431ad04ba480b6d55891beac72e21a
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-08-01 23:34:53 +0600

    delete api added for specific todos delete

--- index.js
+++ index.js
@@ -62,9 +62,25 @@ async function run() {
     });
 
     // get todos by specific user email
+    // app.get("/todos/:email", async (req, res) => {
+    //   const email = req.params.email;
+    //   const result = await todosCollection.findOne({ email: email });
+    //   res.send(result);
+    // });
+
+    // geting all todos by specific user email
     app.get("/todos/:email", async (req, res) => {
       const email = req.params.email;
-      const result = await todosCollection.findOne({ email: email });
+      const cursor = todosCollection.find({ email: email });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //deleting a todo by apecific id
+    app.delete("/todos/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await todosCollection.deleteOne(query);
       res.send(result);
     });
 
